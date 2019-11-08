41fd454f04b3f9ce3ff548d642af03694c77a458
kyr0 <info@aron-homberg.de>
2019-11-08 07:52:07 +0100

    chore: fixed shebang

--- src/cli.ts
+++ src/cli.ts
@@ -1,17 +1,19 @@
 #!/usr/bin/env node
-
-import {mkdir} from "./function/st-mkdir";
 import chalk from "chalk";
+import { mkdir } from "./function/st-mkdir";
 
 const directoriesToCreate = process.argv.slice(2);
 
 (async () => {
-    if (directoriesToCreate.length == 0) {
-        console.log(chalk.red('Nothing to delete.'));
-    } else {
-        console.log(chalk.green('Start creating directories:'), directoriesToCreate);
-        for (let directory of directoriesToCreate) {
-            mkdir(directory);
-        }
+  if (directoriesToCreate.length == 0) {
+    console.log(chalk.red("Nothing to delete."));
+  } else {
+    console.log(
+      chalk.green("Start creating directories:"),
+      directoriesToCreate
+    );
+    for (let directory of directoriesToCreate) {
+      mkdir(directory);
     }
+  }
 })();
