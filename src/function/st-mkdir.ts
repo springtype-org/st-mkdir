6078c4fbca8afdb048dbafdb09fcf9650a8fa572
Michael Mannseicher <m.mansi1@gmx.net>
2019-12-19 01:08:27 +0100

    add: added silent mode no console log

--- src/function/st-mkdir.ts
+++ src/function/st-mkdir.ts
@@ -2,12 +2,17 @@ import chalk from "chalk";
 import { platform } from "os";
 import { mkdir as mkdirWindows } from "./st-mkdir-windows";
 import { mkdir as mkdirLinux } from "./st-mkdir-linux";
+import { IMkdirOptions } from "../interface/imkdiroptions";
+
+ const DEFAULT_OPTIONS: IMkdirOptions = {silent: false};
 
 export const DIR_WILDCARD = "%%_dir_%%";
 
-export const mkdir = (dir: string) => {
-   
-   console.log(chalk.cyan("creating directory: ") + dir);
+export const mkdir = (dir: string, options: IMkdirOptions = DEFAULT_OPTIONS) => {
+
+   if(!options.silent) {
+      console.log(chalk.cyan("creating directory: ") + dir);
+   }
 
    const isWindows = platform() === "win32";
 
---
+++ src/interface/imkdiroptions.ts
@@ -0,0 +1,3 @@
+export interface IMkdirOptions {
+    silent?: boolean
+}
